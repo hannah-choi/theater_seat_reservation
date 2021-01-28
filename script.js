5bab39f620415fbd2598d527c795efcb79d2c100
hannah-developer <hannahchoi.developer@gmail.com>
2021-01-28 11:06:42 +0000

    code refactoring

--- script.js
+++ script.js
@@ -1,37 +1,26 @@
 const selectSeat = document.querySelector(".selectSeat");
 const select = document.querySelector("select");
-const seats = selectSeat.querySelectorAll(".seat");
+const seats = selectSeat.querySelectorAll(".seat:not(.occupied)");
 const seatTotal = document.querySelector(".seatTotal");
 const priceTotal = document.querySelector(".priceTotal");
 
-let cost = 12;
-let numberOfSeats = 0;
+let cost = +select.value;
 
 select.addEventListener("change", e => {
-    cost = parseInt(e.target.value);
-    numberOfSeats = 0;
-    updateNumbers();
+    cost = +e.target.value;
     seats.forEach(seat => seat.classList.remove("selected"));
+    updateNumbers();
 });
 
 const updateNumbers = () => {
-    seatTotal.innerHTML = numberOfSeats;
-    priceTotal.innerHTML = cost * numberOfSeats;
+    let numberOfSeats = document.querySelectorAll(".row .seat.selected");
+    seatTotal.innerHTML = numberOfSeats.length;
+    priceTotal.innerHTML = cost * numberOfSeats.length;
 };
 
-seats.forEach(seat => {
-    seat.addEventListener("click", ({ target }) => {
-        if (target.classList.contains("occupied")) {
-            return;
-        }
-        if (!target.classList.contains("selected")) {
-            target.classList.add("selected");
-            numberOfSeats++;
-            updateNumbers();
-        } else {
-            target.classList.remove("selected");
-            numberOfSeats--;
-            updateNumbers();
-        }
-    });
+selectSeat.addEventListener("click", ({ target }) => {
+    if (target.tagName === "BUTTON" && !target.classList.contains("occupied")) {
+        target.classList.toggle("selected");
+        updateNumbers();
+    }
 });
