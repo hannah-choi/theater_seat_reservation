b11ac6e979542f6a3d1084a9d2b48eb33b9574a1
hannah-developer <hannahchoi.developer@gmail.com>
2021-01-30 22:41:54 +0000

    save the seats in the local storage and load them

--- script.js
+++ script.js
@@ -6,20 +6,52 @@ const priceTotal = document.querySelector(".priceTotal");
 
 let cost = +select.value;
 
-const updateNumbers = () => {
-    let numberOfSeats = selectSeat.querySelectorAll(".seat.selected");
+getData();
+
+const calculate = () => {
+    let numberOfSeats = selectSeat.querySelectorAll(".selected");
+
+    const selectedSeatsIndex = [...numberOfSeats].map(seat =>
+        [...seats].indexOf(seat)
+    );
+
+    localStorage.setItem("selectedSeats", JSON.stringify(selectedSeatsIndex));
+
     seatTotal.innerHTML = numberOfSeats.length;
     priceTotal.innerHTML = numberOfSeats.length * cost;
+
+    const newArray = Array.from(numberOfSeats);
+    console.log(newArray);
+};
+
+const setFilmInfo = (index, price) => {
+    localStorage.setItem("titleIndex", JSON.stringify(index));
+    localStorage.setItem("price", JSON.stringify(price));
+};
+
+const getData = () => {
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add("selected");
+            }
+        });
+    }
 };
 
 select.addEventListener("change", ({ target }) => {
     cost = +target.value;
-    updateNumbers();
+    calculate();
+    setFilmInfo(target.selectedIndex, target.value);
 });
 
 selectSeat.addEventListener("click", ({ target }) => {
-    if (target.tagName === "BUTTON" && !target.classList.contains("occupied")) {
+    if (
+        target.tagName === "BUTTON" &&
+        !target.classList.contains(".occupied")
+    ) {
         target.classList.toggle("selected");
-        updateNumbers();
+        calculate();
     }
 });
